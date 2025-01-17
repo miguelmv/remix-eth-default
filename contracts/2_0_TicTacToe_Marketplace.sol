2a826cc76de5b85d13bbe1101062b6dd732e00e8
miguelmv <miguel.medina.villegas@hotmail.com>
2025-01-16 22:25:27 -0700

    TicTacToe - Resolviendo el modelo de subasta (https://platzi.com/home/clases/7956-solidity-practico/63467-rsolviendo-el-modelo-de-subasta/)

--- contracts/2_0_TicTacToe_Marketplace.sol
+++ contracts/2_0_TicTacToe_Marketplace.sol
@@ -3,10 +3,12 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract TicTacToeMarketplace  {
+contract TicTacToeMarketplace is Ownable(msg.sender)  {
     mapping (uint => uint) valores;
+    mapping (uint => address) postor;
     IERC721 achievements;
     IERC20 moneda;
 
@@ -20,7 +22,28 @@ contract TicTacToeMarketplace  {
         require(valor > 0);
         require(achievements.getApproved(tokenId) == address(this));
         valores[tokenId] = valor;
+        postor[tokenId] = msg.sender;
     }
+
+    function finalizacion(uint tokenId) public onlyOwner {
+        require(valores[tokenId] > 0);
+        require(moneda.allowance(postor[tokenId], address(this)) >= valores[tokenId]);
+        require(achievements.getApproved(tokenId) == address(this));
+
+        moneda.transferFrom(postor[tokenId], achievements.ownerOf(tokenId), valores[tokenId]);
+        achievements.safeTransferFrom(achievements.ownerOf(tokenId), postor[tokenId], tokenId);
+        valores[tokenId] = 0;
+
+    }
+    function ofertar(uint tokenId, uint cantidad) public {
+        require(valores[tokenId] > 0);
+        require(cantidad > valores[tokenId]);
+        require(moneda.allowance(msg.sender, address(this)) > cantidad);
+        postor[tokenId] = msg.sender;
+        valores[tokenId] = cantidad;
+
+    }
+    /*
     function comprar(uint tokenId) public {
         require(valores[tokenId] > 0);
         require(moneda.allowance(msg.sender, address(this)) >= valores[tokenId]);
@@ -29,4 +52,5 @@ contract TicTacToeMarketplace  {
         achievements.safeTransferFrom(achievements.ownerOf(tokenId), msg.sender, tokenId);
         valores[tokenId] = 0;
     }
+    */
 }
